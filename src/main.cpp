4c266e25a9413ba5736adc79b6d9655817d0956c
Venkatesh B <venkateshvgl3004@gmail.com>
2026-07-19 11:24:35 +0530

    removed redundant code in triangle drawing

--- src/main.cpp
+++ src/main.cpp
@@ -86,11 +86,6 @@ int main()
 
   unsigned int VBO;
   glGenBuffers(1, &VBO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-  glEnableVertexAttribArray(0);
   
   unsigned int VAO;
   glGenVertexArrays(1, &VAO);
